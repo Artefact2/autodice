2bddc9bddad0fab3af59e787524a9645b149c85f
Artefact2 <artefact2@gmail.com>
2016-11-11 20:57:10 +0100

    Score matches by distance and by distance difference.

--- autofairdice.py
+++ autofairdice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 import sys
+import math
 import numpy as np
 import cv2
 
@@ -32,34 +33,67 @@ def findHomographyAndInliers(kp1, kp2, matches):
     srcPoints = np.float32([ kp1[m.queryIdx].pt for (m, n) in matches ]).reshape(-1, 1, 2)
     dstPoints = np.float32([ kp2[m.trainIdx].pt for (m, n) in matches ]).reshape(-1, 1, 2)
 
-    M, mask = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC, 25.0)
+    M, mask = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC, 10.0)
     if mask is None:
         return [], [], 0
     
     matchesMask = [ [k,0] for k in mask.ravel().tolist()]
     return M, matchesMask, np.count_nonzero(mask)
 
-def refmatch(img, refs):
+def distanceToCenter(x, y, w, h):
+    dx = x - .5 * (w-1)
+    dy = y - .5 * (h-1)
+    return math.sqrt(dx*dx + dy*dy)
+
+def scoreMatches(matches, mask, kp1, w1, h1, kp2, w2, h2):
+    score = 0.0
+    
+    for i, (m, n) in enumerate(matches):
+        if not mask[i][0]:
+            continue
+        
+        pt1 = kp1[m.queryIdx].pt
+        pt2 = kp2[m.trainIdx].pt
+        r1 = distanceToCenter(pt1[0], pt1[1], w1, h1)
+        r2 = distanceToCenter(pt2[0], pt2[1], w2, h2)
+
+        score += 1.0 / (1.0 + max(0, abs(r1 / 30.0) - 1)) / (1.0 + max(0, abs((r2-r1) / 10.0) - 1))
+        
+    return score
+
+def refmatch(img, refdata):
     kp, des = keypointsAndDescriptors(img)
     
     inl = []
-    for i in refs:
-        matches = matched(refs[i][1], des)
-        if len(matches) > 10:
-            M, matchesMask, nInliers = findHomographyAndInliers(refs[i][0], kp, matches)
-            inl.append((nInliers, i))
+    for i in refdata:
+        matches = matched(refdata[i][0][1], des)
+        if len(matches) <= 10:
+            continue
+        
+        M, matchesMask, nInliers = findHomographyAndInliers(refdata[i][0][0], kp, matches)
+
+        if nInliers <= 10:
+            continue
+
+        h1 = refdata[i][1][0]
+        w1 = refdata[i][1][1]
+        h2, w2, d = img.shape
+        
+        inl.append((scoreMatches(matches, matchesMask, refdata[i][0][0], w1, h1, kp, w2, h2), i))
 
     inl = sorted(inl, reverse = True)
 
     if len(inl) > 1:
-        if inl[0][0] > 2 * inl[1][0]:
+        if inl[0][0] > 1.5 * inl[1][0]:
             return inl[0][1]
         else:
+            print(inl)
             return inl[0][1] + '?'
+            
     elif len(inl) > 0:
-        return inl[0][1] + '??'
+        return inl[0][1]
     else:
-        return '?'
+        return '???'
 
 if len(sys.argv) == 1:
     usage()
@@ -81,21 +115,25 @@ elif sys.argv[1] == "match-test":
     img1 = cv2.imread(sys.argv[2], -1)
     img2 = cv2.imread(sys.argv[3], -1)
 
+    h1, w1, d = img1.shape
+    h2, w2, d = img2.shape
+
     kp1, des1 = keypointsAndDescriptors(img1)
     kp2, des2 = keypointsAndDescriptors(img2)
     
     matches = matched(des1, des2)
     M, matchesMask, nInliers = findHomographyAndInliers(kp1, kp2, matches)
 
-    print("%d matches, %d inliers" % (len(matches), nInliers))
+    print("%d matches, %d inliers, final score %f" % (
+        len(matches), nInliers, scoreMatches(matches, matchesMask, kp1, w1, h2, kp2, w2, h2)
+    ))
 
     if nInliers > 0:
-        h, w, d = img2.shape
-        img4 = cv2.warpPerspective(img1, M, (w, h))
-        img2 = cv2.addWeighted(img2, .7, img4, .3, 0)
+        img4 = cv2.warpPerspective(img1, M, (w2, h2))
+        img5 = cv2.addWeighted(img2, .7, img4, .3, 0)
 
     img3 = cv2.drawMatchesKnn(
-        img1, kp1, img2, kp2, matches, None,
+        img1, kp1, img5, kp2, matches, None,
         matchesMask = matchesMask,
         singlePointColor = (0, 0, 255),
         matchColor = (0, 255, 0),
@@ -107,15 +145,18 @@ elif sys.argv[1] == "match-test":
 elif sys.argv[1] == "match-ref":
     import glob
     
-    refs = dict()
+    refdata = dict()
 
     for reffile in glob.glob('ref/*.png'):
         i = reffile.split('/')[1].split('.')[0]
         img = cv2.imread(reffile, -1)
-        refs[i] = keypointsAndDescriptors(img)
+        refdata[i] = (
+            keypointsAndDescriptors(img),
+            img.shape,
+        )
 
     for i in xrange(2, len(sys.argv)):
-        print "%s %s" % (sys.argv[i], refmatch(cv2.imread(sys.argv[i], -1), refs))
+        print "%s %s" % (sys.argv[i], refmatch(cv2.imread(sys.argv[i], -1), refdata))
     
 else:
     usage()
