095a6bb01e8b33ad12a5687cde9d7e972a15cb0d
Artefact2 <artefact2@gmail.com>
2016-11-11 18:13:43 +0100

    Use findHomography to match pictures.

--- autofairdice.py
+++ autofairdice.py
@@ -11,7 +11,6 @@ def usage():
     print("Usage:")
     print("%s autocrop <in.png> <out.png>" % sys.argv[0])
     print("%s autocrop-test <in.png>" % sys.argv[0])
-    print("%s preprocess-ref" % sys.argv[0])
     print("%s match-test <A.png> <B.png>" % sys.argv[0])
     print("%s match-ref <image.png> ..." % sys.argv[0])
     sys.exit()
@@ -27,22 +26,40 @@ def keypointsAndDescriptors(img):
 
 def matched(des1, des2):
     matches = bf.knnMatch(des1, des2, k = 2)
-    return [ (m, n) for (m, n) in matches if m.distance < .7 * n.distance ]
+    return [ (m, n) for (m, n) in matches if m.distance < .8 * n.distance ]
 
-def refmatch(img, ref):
+def findHomographyAndInliers(kp1, kp2, matches):
+    srcPoints = np.float32([ kp1[m.queryIdx].pt for (m, n) in matches ]).reshape(-1, 1, 2)
+    dstPoints = np.float32([ kp2[m.trainIdx].pt for (m, n) in matches ]).reshape(-1, 1, 2)
+
+    M, mask = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC, 25.0)
+    if mask is None:
+        return [], [], 0
+    
+    matchesMask = [ [k,0] for k in mask.ravel().tolist()]
+    return M, matchesMask, np.count_nonzero(mask)
+
+def refmatch(img, refs):
     kp, des = keypointsAndDescriptors(img)
     
-    goodmatches = []
-    for i in refs.files:
-        matches = matched(des, refs[i])
-        goodmatches.append((len(matches), i))
+    inl = []
+    for i in refs:
+        matches = matched(refs[i][1], des)
+        if len(matches) > 10:
+            M, matchesMask, nInliers = findHomographyAndInliers(refs[i][0], kp, matches)
+            inl.append((nInliers, i))
 
-    goodmatches = sorted(goodmatches, reverse = True)
-    
-    if goodmatches[0][0] > 1.5 * goodmatches[1][0]:
-        return goodmatches[0][1]
+    inl = sorted(inl, reverse = True)
+
+    if len(inl) > 1:
+        if inl[0][0] > 2 * inl[1][0]:
+            return inl[0][1]
+        else:
+            return inl[0][1] + '?'
+    elif len(inl) > 0:
+        return inl[0][1] + '??'
     else:
-        return goodmatches[0][1] + '?'
+        return '?'
 
 if len(sys.argv) == 1:
     usage()
@@ -58,39 +75,47 @@ elif sys.argv[1] == "autocrop-test":
     plt.imshow(cv2.cvtColor(autocrop(img), cv2.COLOR_BGR2RGB))
     plt.show()
 
-elif sys.argv[1] == "preprocess-ref":
-    import glob
-
-    descriptors = dict()
-
-    for reffile in glob.iglob('ref/*.png'):
-        img = cv2.imread(reffile, 0)
-        sift = cv2.xfeatures2d.SIFT_create()
-        kp, des = sift.detectAndCompute(img, None)
-        descriptors[reffile.split('/')[1].split('.')[0]] = des
-
-    np.savez('ref-descriptors.npz', **descriptors)
-
 elif sys.argv[1] == "match-test":
     from matplotlib import pyplot as plt
     
-    img1 = cv2.imread(sys.argv[2], 0)
-    img2 = cv2.imread(sys.argv[3], 0)
+    img1 = cv2.imread(sys.argv[2], -1)
+    img2 = cv2.imread(sys.argv[3], -1)
 
     kp1, des1 = keypointsAndDescriptors(img1)
     kp2, des2 = keypointsAndDescriptors(img2)
+    
     matches = matched(des1, des2)
+    M, matchesMask, nInliers = findHomographyAndInliers(kp1, kp2, matches)
 
-    img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None)
+    print("%d matches, %d inliers" % (len(matches), nInliers))
+
+    if nInliers > 0:
+        h, w, d = img2.shape
+        img4 = cv2.warpPerspective(img1, M, (w, h))
+        img2 = cv2.addWeighted(img2, .7, img4, .3, 0)
+
+    img3 = cv2.drawMatchesKnn(
+        img1, kp1, img2, kp2, matches, None,
+        matchesMask = matchesMask,
+        singlePointColor = (0, 0, 255),
+        matchColor = (0, 255, 0),
+    )
 
     plt.imshow(cv2.cvtColor(img3, cv2.COLOR_BGR2RGB))
     plt.show()
 
 elif sys.argv[1] == "match-ref":
-    refs = np.load('ref-descriptors.npz')
+    import glob
+    
+    refs = dict()
+
+    for reffile in glob.glob('ref/*.png'):
+        i = reffile.split('/')[1].split('.')[0]
+        img = cv2.imread(reffile, -1)
+        refs[i] = keypointsAndDescriptors(img)
 
     for i in xrange(2, len(sys.argv)):
-        print "%s %s" % (sys.argv[i], refmatch(cv2.imread(sys.argv[i], 0), refs))
+        print "%s %s" % (sys.argv[i], refmatch(cv2.imread(sys.argv[i], -1), refs))
     
 else:
     usage()
